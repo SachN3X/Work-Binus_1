86cfe328716fe41a13c5c4b78dd4ba1935681ed8
SachN3X <sachikolucia13@gmail.com>
2024-12-07 14:46:37 +0700

    Create 07.12.2024 (Exercise 2).py

---
+++ 07.12.2024 (Exercise 2).py
@@ -0,0 +1,8 @@
+while(True):
+#input float
+   
+    repeat = input("Do you want to repeat? (answer using Y/N) =")
+    if (repeat == "N"):
+        print ("Program Stops")
+        print ("Thank you for using my program ^^")
+        break
