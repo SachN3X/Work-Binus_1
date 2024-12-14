21541552769a9fbbb075e3180da6200074729faf
SachN3X <sachikolucia13@gmail.com>
2024-12-14 14:49:29 +0700

    Update Exercise 5.py

--- Exercise 5.py
+++ Exercise 5.py
@@ -7,7 +7,7 @@ if a == 0:
     if a == 0:
      print("Bukan persamaan kuadrat.")
 else:
-    print(f"Persamaan kuadrat:"+ str(a) + "x² + " + str(b) + "x + " + str(c) + " = 0")
+    print("Persamaan kuadrat:"+ str(a) + "x² + " + str(b) + "x + " + str(c) + " = 0")
     D = b**2 - 4*a*c
     print("Determinannya:"+ str(D))
     if D > 0:
