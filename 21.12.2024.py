93907d5801ed8b552cb7d1a07599c83e1446c3e0
SachN3X <sachikolucia13@gmail.com>
2024-12-21 14:29:21 +0700

    Create 21.12.2024.py

---
+++ 21.12.2024.py
@@ -0,0 +1,23 @@
+class Student:
+    "Common base class for all student"
+    rollNum = 0
+    
+    def __init__(self, name="Student",age=0,NIK=0):
+        self.name = name
+        self.age = age
+        self.NIK = NIK
+        Student.rollNum += 1
+        
+    def displayCount(self):
+        print("Total Student: %d" % Student.rollNum)
+    
+    def printStudent(self):
+        print("Name:", self.name, "\nage:", self.age, "\nNIK",self.NIK)
+        
+Name = str(input("Enter your Name:"))
+Age = str(input("Enter your Age:"))
+NIK = str(input("Enter your NIK:"))
+
+Student = Student(Name, Age, NIK)
+Student.displayCount()
+Student.printStudent()
