eaf24202fe94d689286c6f820219caae03db3708
SachN3X <sachikolucia13@gmail.com>
2024-12-21 14:35:17 +0700

    Update 21.12.2024.py

--- 21.12.2024.py
+++ 21.12.2024.py
@@ -2,17 +2,17 @@ class Student:
     "Common base class for all student"
     rollNum = 0
     
-    def __init__(self, name="Student",age=0,NIK=0):
+    def __init__(self, name="Student",age=0,nik=0):
         self.name = name
         self.age = age
-        self.NIK = NIK
+        self.nik = NIK
         Student.rollNum += 1
         
     def displayCount(self):
         print("Total Student: %d" % Student.rollNum)
     
     def printStudent(self):
-        print("Name:", self.name, "\nage:", self.age, "\nNIK",self.NIK)
+        print("Name:", self.name, "\nage:", self.age, "\nnik",self.nik)
         
 Name = str(input("Enter your Name:"))
 Age = str(input("Enter your Age:"))
