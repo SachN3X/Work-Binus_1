1051b38f9b3e05fefc36f4043e2bb69ec8dc6521
SachN3X <sachikolucia13@gmail.com>
2025-01-12 16:49:23 +0700

    Create UJIAN_1.py

---
+++ UJIAN_1.py
@@ -0,0 +1,5 @@
+A =float(input('Masukan nilai untuk variabel A = ')) # untuk input nilai dari variabel A
+B =float(input('Masukan nilai untuk variabel B = ')) #untuk input nilai dari variabel B
+V =float(input('Masukan nilai untuk variabel V = ')) #untuk input nilai dari variabel V
+hasil = ( (B-10) / ( 2 * ( 1 - ( A / ( 2 * V ** 2) ) ) ) )
+print ("Hasil dari nilai nilai yang telah diinput adalah", hasil)
